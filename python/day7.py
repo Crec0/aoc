e8a8c0ccae9b9eca862c3e0556921ab23743016b
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-12-08 12:48:49 -0800

    chore: time day 7

--- python/day7.py
+++ python/day7.py
@@ -1,6 +1,8 @@
 import math
 import statistics
 
+import util
+
 
 def read_input(file_name: str) -> list[int]:
     with open(file_name, 'r') as file:
@@ -8,21 +10,19 @@ def read_input(file_name: str) -> list[int]:
     return [int(elem) for elem in data.split(",")]
 
 
+@util.time_it_and_evaluate
 def solve_1(data: list[int]) -> int:
     cheapest = statistics.median(data)
     return sum(abs(elem - cheapest) for elem in data)
 
 
+@util.time_it_and_evaluate
 def solve_2(data: list[int]) -> int:
     pos = math.floor(statistics.mean(data))
     return sum(map(lambda n: n / 2 * (n + 1), map(lambda e: abs(e - pos), data)))
 
 
 if __name__ == '__main__':
-    # input = read_input('../data/day7/input_example.txt')
     input = read_input('../data/day7/input.txt')
-    ans_1 = solve_1(input)
-    print(f"{ans_1 = }")
-
-    ans_2 = solve_2(input)
-    print(f"{ans_2 = }")
+    solve_1(input)
+    solve_2(input)
